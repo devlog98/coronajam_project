1f4c0ab3b24dbaa052e083bf2edc256b6895f165
devlog98 <dno1998@hotmail.com>
2020-03-25 23:27:41 -0300

    [ADD] Merging MainMenu

---
+++ Assets/Code/MainMenu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+
+
+{
+    public int gameSceneIndex;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameSceneIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
