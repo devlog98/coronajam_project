c11aa44be13d68293916e35d152ca217e08b3b5a
devlog98 <dno1998@hotmail.com>
2020-03-26 00:24:47 -0300

    [ADD] Beginning of Round of Battle

---
+++ Assets/Code/Enemy/EnemyDifficulty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class EnemyDifficulty {
+    [SerializeField] private float speedSide;
+    [SerializeField] private float speedUp;
+    [SerializeField] private float timeMovement;
+    [SerializeField] private float timeAttack;
+    [SerializeField] private bool virusShot;
+
+    //getters
+    public float SpeedSide { get => speedSide; }
+    public float SpeedUp { get => speedUp; }
+    public float TimeMovement { get => timeMovement; }
+    public float TimeAttack { get => timeAttack; }
+    public bool VirusShot { get => virusShot; }
+}
---
+++ Assets/Code/Round/Round.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Round {
+    [SerializeField] private Dialogue dialogue;
+    [SerializeField] private EnemyDifficulty enemyDifficulty;
+
+    public Dialogue Dialogue { get => dialogue; }
+    public EnemyDifficulty EnemyDifficulty { get => enemyDifficulty; }
+}
