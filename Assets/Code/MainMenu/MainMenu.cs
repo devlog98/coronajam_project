5e2fd60ae1bfa2ecc3b30dbc46c4e1f28d660689
devlog98 <dno1998@hotmail.com>
2020-03-28 16:20:09 -0300

    [ADD] Main Menu sound effects

--- Assets/Code/MainMenu/MainMenu.cs
+++ Assets/Code/MainMenu/MainMenu.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
 
+public class MainMenu : MonoBehaviour {
+    public int gameSceneIndex; //loads new game scene
+    public Animator anim; //reference to canvas animator
 
-{
-    public int gameSceneIndex;
+    public void PlayGame() {
+        //hides and locks mouse to avoid repeated clicks
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(gameSceneIndex);
+        //start fade out
+        StartCoroutine(PlayGameCoroutine());
     }
 
-    public void Quit()
-    {
+    public void Quit() {
         Application.Quit();
     }
 
+    //coroutine responsible for fade out of menu
+    private IEnumerator PlayGameCoroutine() {
+        anim.SetTrigger("StartGame"); //activates animation
+        yield return null; //skips frame so that animator info is updated with new activated animation
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length); //waits duration on fade out animation
+        SceneManager.LoadScene(gameSceneIndex); //goes to scene
+    }
 }
---
+++ Assets/Code/MainMenu/MainMenuButton.cs
@@ -0,0 +1,19 @@
+﻿using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButton : MaskableGraphic, IPointerEnterHandler {
+    [EventRef] [SerializeField] private string buttonHighlightSound = "event:/MPass";
+    [EventRef] [SerializeField] private string buttonSelectSound = "event:/MSelect";
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        AudioManager.instance.PlayAudioclip(buttonHighlightSound);
+    }
+
+    public void OnClick() {
+        AudioManager.instance.PlayAudioclip(buttonSelectSound);
+    }
+}
