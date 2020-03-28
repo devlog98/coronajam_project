314abdb8a85f05e413a2455b1fb98aa3dcf7226e
devlog98 <dno1998@hotmail.com>
2020-03-28 17:54:03 -0300

    [MOD] Developing battle

--- Assets/Code/Dialogue/DialogueChoice.cs
+++ Assets/Code/Dialogue/DialogueChoice.cs
@@ -7,8 +7,10 @@ using UnityEngine;
 public class DialogueChoice {
     [SerializeField] string choiceKey; //the key in order to localize choice
     [SerializeField] int choiceValue; //the value of choice (can harm your enemy or cure him)
+    [SerializeField] string choiceReactionKey; //the key in order to localize reaction of enemy
 
     //getters
     public string ChoiceKey { get => choiceKey; }
     public int ChoiceValue { get => choiceValue; }
+    public string ChoiceReactionKey { get => choiceReactionKey; }
 }
