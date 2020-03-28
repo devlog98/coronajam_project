eb486cab256b843b748cf3dea383c4f7e58d45c2
devlog98 <dno1998@hotmail.com>
2020-03-27 22:52:29 -0300

    [MOD] Updated PlayerUI

--- Assets/Code/Player/PlayerUI.cs
+++ Assets/Code/Player/PlayerUI.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerUI : MonoBehaviour {
-    [SerializeField] GameObject healthTemplate;
+    [SerializeField] GameObject healthIconTemplate;
     [SerializeField] GameObject healthContainer;
-    [SerializeField] float healthDistance = 0.5f;
+    [SerializeField] float healthIconDistance = 0.5f;
+
+    private List<GameObject> healthIcons = new List<GameObject>();
 
     //start health counter with specific number of health
-    public void StartHealthCounter(int health) {
-        for(int i = 0; i < health; i++) {
+    public void StartHealthCounter(int currentHealth) {
+        for (int i = 0; i < currentHealth; i++) {
             GameObject newHealth = null;
 
             //instantiate new health icon
-            newHealth = Instantiate(healthTemplate, healthContainer.transform);
+            newHealth = Instantiate(healthIconTemplate, healthContainer.transform);
             newHealth.name = "Heart " + i;
 
             //positioning new health
             RectTransform newHealthTransform = newHealth.GetComponent<RectTransform>();
-            newHealthTransform.anchoredPosition = new Vector2(healthDistance * i, 0);
+            newHealthTransform.anchoredPosition = new Vector2(healthIconDistance * i, 0);
+
+            //add object to list in order to keep track of changing lifes
+            healthIcons.Add(newHealth);
         }
 
-        healthTemplate.SetActive(false); //hides original template
+        healthIconTemplate.SetActive(false); //hides original template
+    }
+
+    public void UpdateHealthCounter(int currentHealth) {
+        int previousHealth = healthIcons.FindAll(x => x.activeInHierarchy).Count;
+
+        if (previousHealth > currentHealth) {
+            //lost health
+        }
+        else if (currentHealth > previousHealth) {
+            //got health
+        }
     }
 }
