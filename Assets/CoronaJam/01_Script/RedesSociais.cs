c69184d442ae82c190c797180998472ed21d51a2
MichaelDouglasPIX <michaeldouglas_sl@hotmail.com>
2020-03-29 20:43:14 -0300

    Créditos
    redes sociais

---
+++ Assets/CoronaJam/01_Script/RedesSociais.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedesSociais : MonoBehaviour
+{
+    [SerializeField] private string redeSocial;
+    // Start is called before the first frame update
+
+    public void openUrl()
+    {
+        Application.OpenURL(redeSocial);
+    }
+}
