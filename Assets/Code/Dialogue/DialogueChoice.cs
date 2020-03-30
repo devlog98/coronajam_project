146b587c2c7f63dedd29f5421cb08fae1542c2b4
Delacroz <50087071+Delacroz@users.noreply.github.com>
2020-03-29 22:12:30 -0300

    Merge

--- Assets/Code/Dialogue/DialogueChoice.cs
+++ Assets/Code/Dialogue/DialogueChoice.cs
@@ -8,9 +8,11 @@ public class DialogueChoice {
     [SerializeField] string choiceKey; //the key in order to localize choice
     [SerializeField] int choiceValue; //the value of choice (can harm your enemy or cure him)
     [SerializeField] string choiceReactionKey; //the key in order to localize reaction of enemy
-
+    [SerializeField] float reactionTime = 3;
     //getters
     public string ChoiceKey { get => choiceKey; }
     public int ChoiceValue { get => choiceValue; }
     public string ChoiceReactionKey { get => choiceReactionKey; }
+
+    public float ReactionTime { get => reactionTime; }
 }
