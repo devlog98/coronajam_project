ab4d7b60d2c2e1d2bfc6f3cb9a91ff8d9bc486bd
Delacroz <50087071+Delacroz@users.noreply.github.com>
2020-03-29 23:43:51 -0300

    Cor dos sprites

---
+++ Assets/Code/MainMenu/CutsceneManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CutsceneManager : MonoBehaviour {
+    [SerializeField] private List<CutsceneSlide> cutscenes;
+    public int sceneIndex;
+    private int slideIndex = 0;
+
+    public Image currentImage;
+    public Text currentName;
+    public Text currentSentence;
+
+    public GameObject nameContainer;
+    public GameObject sentenceContainer;
+
+    private void Start() {
+        currentImage.sprite = cutscenes[0].Sprite;
+        AudioManager.instance.PlayAudioclip(cutscenes[0].SoundEffect);
+
+        if (String.IsNullOrEmpty(cutscenes[0].Name)) {
+            nameContainer.SetActive(false);
+        }
+        else {
+            nameContainer.SetActive(true);
+            currentName.text = cutscenes[0].Name;
+        }
+
+        if (String.IsNullOrEmpty(cutscenes[0].Sentence)) {
+            sentenceContainer.SetActive(false);
+        }
+        else {
+            sentenceContainer.SetActive(true);
+            currentSentence.text = cutscenes[0].Sentence;
+        }
+    }
+
+    public void NextImageCutscene() {
+        slideIndex++;
+
+        if (slideIndex < cutscenes.Count) {
+            currentImage.sprite = cutscenes[slideIndex].Sprite;
+            AudioManager.instance.PlayAudioclip(cutscenes[slideIndex].SoundEffect);
+
+            if (String.IsNullOrEmpty(cutscenes[slideIndex].Name)) {
+                nameContainer.SetActive(false);
+            } else {
+                nameContainer.SetActive(true);
+                currentName.text = cutscenes[slideIndex].Name;
+            }
+
+            if (String.IsNullOrEmpty(cutscenes[slideIndex].Sentence)) {
+                sentenceContainer.SetActive(false);
+            }
+            else {
+                sentenceContainer.SetActive(true);
+                currentSentence.text = cutscenes[slideIndex].Sentence;
+            }
+        }
+        else {
+            JumpIntro();
+        }
+    }
+
+    public void JumpIntro() {
+        MusicManager.instance.StopEvent();
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Assets/Code/MainMenu/CutsceneSlide.cs
@@ -0,0 +1,18 @@
+﻿using FMODUnity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CutsceneSlide {
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private string name;
+    [SerializeField] private string sentence;
+    [SerializeField] [EventRef] private string soundEffect;
+
+    public Sprite Sprite { get => sprite; }
+    public string Name { get => name; }
+    public string Sentence { get => sentence; }
+    public string SoundEffect { get => soundEffect; }
+}
