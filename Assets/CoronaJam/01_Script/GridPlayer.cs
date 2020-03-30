09e30cb7363e29d766bd1f1fb02d4d708dd1d101
MichaelDouglasPIX <michaeldouglas_sl@hotmail.com>
2020-03-29 22:36:48 -0300

    Feedback grid attack

---
+++ Assets/CoronaJam/01_Script/GridPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPlayer : MonoBehaviour
+{
+    public GameObject spawnObject;
+    GameObject tiledestroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Attack"))
+        {
+            /*GameObject spawVirus*/ tiledestroy = Instantiate(spawnObject, transform.position, transform.rotation);
+        }
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Destroy(tiledestroy);
+    }
+}
