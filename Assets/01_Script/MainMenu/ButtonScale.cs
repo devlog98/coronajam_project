3deaad7d4d118c99bf2911c39aebd52d51e5b5d8
Michael Douglas <44294286+MichaelDouglasPIX@users.noreply.github.com>
2020-09-16 14:50:08 -0300

    Adjust button width
    ButtonScale:
    - Adjust button size according to language

---
+++ Assets/01_Script/MainMenu/ButtonScale.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScale : MonoBehaviour
+{
+    [Header("Settings to Button")]
+    [SerializeField] private Text myText;
+    [SerializeField] private float widthPT;
+    [SerializeField] private float widthEn;
+    private RectTransform width;
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = this.GetComponent<RectTransform>();
+        Size();   
+    }
+
+    private void Size()
+    {
+        //If temporario, se possivel obter a linguagem atual para utilizar no if
+        if (myText.text == "VOLTAR PARA PARTIDA" || myText.text == "MENU PRINCIPAL")
+        {
+            width.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, widthPT);
+        }
+        else
+        {
+            width.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, widthEn);
+        }
+    }
+}
